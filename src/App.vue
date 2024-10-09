d8cf8a6dbe2e261b6981d71aa93fb22832b8e259
Dani <daniel.palma.1345@gmail.com>
2024-10-09 20:52:55 -0300

    [Fix] - Fixes based on internal js and vue issues

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
-import ButtonWrapper from "@/components/ButtonWrapper.vue";
+import ButtonWrapper from "src/components/ButtonWrapper.vue";
 </script>
 
 <template>
-  <header>
-
-  </header>
-
   <main>
     <ButtonWrapper />
   </main>
